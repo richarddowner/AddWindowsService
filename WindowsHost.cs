5924d9da7b41d4d4dedc03f87c08e37b37cf5d34
Richard Downer <richard.downer@xero.com>
2014-02-20 14:23:01 +1300

    Init

---
+++ WindowsHost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace AddWindowsService
+{
+    class WindowsHost : ServiceBase
+    {
+        public WindowsHost()
+        {
+            ServiceName = ConfigurationSettings.AppSettings["ServiceName"];
+            CanStop = Boolean.Parse(ConfigurationSettings.AppSettings["CanStop"]);
+            CanPauseAndContinue = Boolean.Parse(ConfigurationSettings.AppSettings["CanPauseAndContinue"]);
+            AutoLog = Boolean.Parse(ConfigurationSettings.AppSettings["AutoLog"]);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Process.Start(ConfigurationSettings.AppSettings["ProcessExe"]);
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                foreach (var proc in Process.GetProcessesByName(ConfigurationSettings.AppSettings["ProcessName"]))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ServiceBase.Run(new WindowsHost());
+        }
+    }
+}
---
+++ WindowsService.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace AddWindowsService
+{
+    [RunInstaller(true)]
+    public class WindowsService : Installer
+    {
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+
+        public WindowsService()
+        {
+            processInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "Change My Name";
+            Installers.Add(serviceInstaller);
+            Installers.Add(processInstaller);
+        }
+    }
+}
